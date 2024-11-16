04b5f5ee3046bcc77cc134d73868334126f3ef69
Kyung-Huni <dludgns12345@gmail.com>
2024-11-16 22:58:03 +0900

    FINAL FINAL

--- store/index.js
+++ store/index.js
@@ -13,7 +13,7 @@ export const actions = {
   async checkSession({ commit }) {
     try {
       const response = await this.$axios.get(
-        'https://leave-tracker-lma6wsk5a-leekyunghuns-projects.vercel.app/api/auth/session-check'
+        'https://leave-tracker-y23h.onrender.com/api/auth/session-check'
       )
       commit('setAuthentication', response.data.isAuthenticated)
     } catch (error) {
