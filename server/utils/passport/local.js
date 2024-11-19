bc49c13a81134b67df191fafe4585014d7ee43f0
Kyung Hun <dludgns12345@gmail.com>
2024-11-19 23:35:32 +0900

    LOCAL MODULE FIXED

--- server/utils/passport/local.js
+++ server/utils/passport/local.js
@@ -1,35 +1,41 @@
 const LocalStrategy = require('passport-local').Strategy
+const passport = require('passport')
 
 const { users } = require('../../models')
 
-module.exports = new LocalStrategy(
-  {
-    usernameField: 'uid',
-    passwordField: 'password',
-    passReqToCallback: true,
-  },
-  function (req, uid, password, done) {
-    if (!uid || !password) return done(null, false)
+module.exports = () => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'uid',
+        passwordField: 'password',
+        passReqToCallback: true,
+      },
+      async (uid, password, done) => {
+        try {
+          const exUser = await users.findOne({ where: { uid: uid }, raw: true })
 
-    users
-      .findOne({
-        where: { uid: uid },
-        raw: true,
-      })
-      .then((result) => {
-        if (!result) {
-          done(null, false)
-        } else {
-          if (result.password == password) {
-            done(null, {
-              uid: result['uid'],
-              id: result['id'],
-              status: result['status'],
-            })
+          if (exUser) {
+            const result = exUser.password == password
+            console.log('Login result: ', result)
+
+            if (result) {
+              done(null, {
+                uid: result['uid'],
+                id: result['id'],
+                status: result['status'],
+              })
+            } else {
+              done(null, false, { message: '비밀번호가 일치하지 않습니다.' })
+            }
           } else {
-            done(null, false)
+            done(null, false, { message: '가입되지 않은 회원입니다.' })
           }
+        } catch (error) {
+          console.error(error)
+          done(error)
         }
-      })
-  }
-)
+      }
+    )
+  )
+}
